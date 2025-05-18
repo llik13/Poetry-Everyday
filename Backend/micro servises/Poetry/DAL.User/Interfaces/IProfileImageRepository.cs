bf8ab5b7da75d9780e661aa560574e8de32da020
Artem Dyminiatsa <murchick2004@gmail.com>
2025-05-18 18:26:25 +0300

    image v2

---
+++ Backend/micro servises/Poetry/DAL.User/Interfaces/IProfileImageRepository.cs
@@ -0,0 +1,11 @@
+﻿using DAL.User.Entities;
+
+namespace DAL.User.Interfaces
+{
+    public interface IProfileImageRepository
+    {
+        Task<bool> SaveImageAsync(ProfileImage image);
+        Task<bool> DeleteImageAsync(string userId);
+        Task<ProfileImage> GetImageByUserIdAsync(string userId);
+    }
+}
